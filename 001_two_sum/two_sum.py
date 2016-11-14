184a2eb819de09365e6c4166c3ddecc81310fafd
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-11-14 14:30:55 -0500

    python ok

--- 001_two_sum/two_sum.py
+++ 001_two_sum/two_sum.py
@@ -1,14 +1,26 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        s = {};
-        for i in range(len(nums)):
-            comp = target - nums[i];
-            if comp in s:
-                return [s[comp], i];
-            s[nums[i]] = i;
-        
+  def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    s = {};
+    for i in range(len(nums)):
+      comp = target - nums[i];
+      if comp in s:
+          return [s[comp], i];
+      s[nums[i]] = i;
+
+
+if __name__ == '__main__':
+  try:
+    while (True):
+      sol = Solution()
+      nums = [int(x) for x in input().split()]
+      target = int(input())
+      ans = sol.twoSum(nums, target)
+      print(ans)
+  except EOFError:
+    pass
+
