ae32b651d430a0419d6ce8c1f8e8dfd2e2e87b5a
Qinmao Zhang <zhangq2@rose-hulman.edu>
2016-11-14 15:55:43 -0500

    javascript working

--- 001_two_sum/two_sum.js
+++ 001_two_sum/two_sum.js
@@ -4,13 +4,35 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    var m = new Map();
-    for (var i = 0; i < nums.length; i ++){
-        var comp = target - nums[i];
-        if (comp in m){
-            return [m[comp] , i];
-        }
-        m[nums[i]] = i;
+  var m = new Map();
+  for (var i = 0; i < nums.length; i ++){
+    var comp = target - nums[i];
+    if (comp in m){
+      return [m[comp] , i];
     }
-    return undefined;
+    m[nums[i]] = i;
+  }
+  return undefined;
 };
+
+if (require.main === module) {
+	const readline = require('readline');
+	const rl = readline.createInterface({
+	  input: process.stdin,
+	  output: process.stdout
+	});
+
+	let nums = undefined;
+	rl.on('line', (input) => {
+		if (!nums) {
+			nums = input.split(" ").map((x) => {
+				return parseInt(x);
+			});
+		} else {
+			let target = parseInt(input);
+			let ans = twoSum(nums, target);
+			console.log(ans);
+			nums = undefined;
+		}
+	});
+}
